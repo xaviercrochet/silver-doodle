cd5668b6329a08c1c4ac2efb15221593e57081f0
unknown <github@xcrochet.net>
2020-10-22 16:33:25 +0200

    removed unused json module

--- json/parser.go
+++
@@ -1,9 +0,0 @@
-package json
-
-// Parse deserialize localsearch response into a LocalSearchPlace struct
-//func Parse(bodyJSON []byte) (*LocalSearchPlace, error) {
-//	place := &LocalSearchPlace{}
-//	err := json.Unmarshal([]byte(bodyJSON), &place)
-//	return place, err
-
-//}
